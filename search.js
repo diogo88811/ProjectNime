7f212f9d5bdef59e06c1f42135bd22dc407df14a
marciojlr <marcioribeiroslb@gmail.com>
2022-11-07 23:55:02 +0000

    image

--- search.js
+++ search.js
@@ -1,3 +1,7 @@
+let disk;
+function preload(){
+    disk = loadImage('Pictures/DJDisk.png');
+}
 function setup() {
     createCanvas(windowWidth, windowHeight);
     input = createInput();
@@ -8,12 +12,11 @@ function setup() {
     searchButton.position(40,40);
     searchButton.mousePressed(searchEvent);
 
-    img = loadImage('Pictures/DJDisk.png');
 
   }
   
 function draw() {
-    image(img, 0, 0)
+    image(disk, (5*windowWidth)/6, windowHeight/4, windowHeight/6, windowHeight/6);
 }
 
 function typeMusicEvent(){
