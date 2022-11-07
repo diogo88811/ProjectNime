4b30e1d448f3277eeef197b6dae7b1d11da5f7c8
Diogo Valente Martins <dvmartins@student.dei.uc.pt>
2022-11-07 22:45:27 +0000

    playMusic

--- Code/sketch.js
+++ Code/sketch.js
@@ -1,12 +1,21 @@
+let song1;
+let song2;
+
+function preload(){
+  soundFormats('mp3', 'ogg');
+  song1 = loadSound('/Musicas/Phoenix_RDC_Dureza_[Video_Oficial].mp3');
+  song2 = loadSound('/Musicas/ProfJam_Água_de_Coco_(Prod_Lhast)[1].mp3');
+}
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-  }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(canvasPressed);
+  background(220);
+  text('tap here to play', 10, 20);
+}
+
+function canvasPressed() {
+  // playing a sound file on a user gesture
+  // is equivalent to `userStartAudio()`
+  song1.play();
+}
