13050002deadb970e921337149d0ac9891f1f1a9
marciojlr <marcioribeiroslb@gmail.com>
2022-11-08 23:45:09 +0000

    animation

--- search.js
+++ search.js
@@ -16,6 +16,9 @@ let song2;
 let queue = [];
 let songNumber;
 var b;
+var amp;
+var volhistory = [];
+
 
 function preload(){
     disk1 = loadImage('Pictures/DJDisk.png');
@@ -32,14 +35,17 @@ function preload(){
 function setup() {
     createCanvas(windowWidth, windowHeight);
     imageMode(CENTER);
-    angleMode(DEGREES);  
+    angleMode(DEGREES);
     rotate1 = 0;
     rotate2 = 0;
 
     //Music
     b = createButton("PLAY")
-    b.position(windowWidth/2, windowHeight/2)
+    b.position(windowWidth/2, windowHeight/2+50)
     b.mousePressed(musicEvent)
+
+    //Animation
+    amp = new p5.Amplitude();
 
     //Transition
     sliderTransition = createSlider(0, 1, 0.5, 0.01);
@@ -50,35 +56,45 @@ function setup() {
     sliderTransition.mousePressed(sliderTransition)
 
     //DISK 1 COMPONENTS
-    sliderRate1 = createSlider(0, 1.5, 1, 0.01);
+    sliderRate1 = createSlider(0, 2, 1, 0.01);
     sliderRate1.position((1.5*windowWidth)/8, 0.1*windowHeight);
     sliderRate1.addClass("mySliders");
 
-    sliderVolume1 = createSlider(0, 1,0.6, 0.01);
+    sliderVolume1 = createSlider(0, 1,1, 0.01);
     sliderVolume1.position((1.5*windowWidth)/8, 0.12*windowHeight);
     sliderVolume1.addClass("mySliders");
 
     //DISK 2 COMPONENTS
-    sliderRate2 = createSlider(0, 1.5, 1, 0.01);
+    sliderRate2 = createSlider(0, 2, 1, 0.01);
     sliderRate2.position((1.5*windowWidth)/8, (1-0.1)*windowHeight);
     sliderRate2.addClass("mySliders");
 
-    sliderVolume2 = createSlider(0, 1,0.6, 0.01);
+    sliderVolume2 = createSlider(0, 1,1, 0.01);
     sliderVolume2.position((1.5*windowWidth)/8, (1-0.12)*windowHeight);
     sliderVolume2.addClass("mySliders");
     
   }
   
 function draw() { 
-    background(120,0,60);
+    background(255,255,255);
+    stroke(200);
+   
+    fill(255,255,255);
+    rect(0, 0, windowWidth/3, windowHeight);
+    
+    
+    fill(255,255,255);
+    rect(windowWidth/3, 0, windowWidth/3, windowHeight);
     
     //UPDATE SOUND
     updateSound(sliderTransition.value(), sliderVolume1.value(),sliderVolume2.value(), sliderRate1.value(), sliderRate2.value());
 
+    //Middle Animation
+    middleAnimation();
     //ROTATE DISKS
-    rotate1_speed = sliderRate1.value()*8;
+    rotate1_speed = sliderRate1.value()*3;
     rotate1 += rotate1_speed;
-    rotate2_speed = sliderRate2.value()*8;
+    rotate2_speed = sliderRate2.value()*3;
     rotate2 += rotate2_speed;
     push()
         translate((2*windowWidth)/8, 0.3*windowHeight)
@@ -124,3 +140,27 @@ function updateSound(transaction, volume1, volume2, rate1, rate2){
     queue[1].rate(rate2);
     
 }
+
+function middleAnimation() {
+    
+    var vol = amp.getLevel();
+    volhistory.push(vol);
+
+    stroke(255);
+    fill(150,0,0);
+    push();
+    translate(width / 2, height / 2);
+    beginShape();
+    for (var i = 0; i < 360; i++) {
+      var r = map(volhistory[i], 0, 1, 50, 500);
+      var x = r * cos(i);
+      var y = r * sin(i);
+      vertex(x, y);
+    }
+    endShape();
+    pop();
+
+    if (volhistory.length > 360) {
+      volhistory.splice(0, 1);
+    }
+  }
